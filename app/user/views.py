e0c171a130f2af14f237b2af29e69561aeb0b42b
Rafik Dev <rafikbdev@gmail.com>
2022-09-06 22:09:30 +0200

    Manage user API Passing test

--- app/user/views.py
+++ app/user/views.py
@@ -23,10 +23,10 @@ class CreateTokenView(ObtainAuthToken):
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manange the authenticated user."""
+    """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permissions_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
