85c46583653f6ae285eb35d549282ee0ce531c21
Rafik Dev <rafikbdev@gmail.com>
2022-09-12 10:32:45 +0200

    Implement recipe listing API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -29,7 +29,7 @@ def create_recipe(user, **params):
     }
     defaults.update(params)
 
-    recipe = Recipe.objects.create(user=user **defaults)
+    recipe = Recipe.objects.create(user=user, **defaults)
     return recipe
 
 
